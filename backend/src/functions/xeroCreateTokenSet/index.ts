c24d317562a98941351b9e49d3079a7f12a3aada
darkhorsecorder <boryschernenko.dream@gmail.com>
2023-12-14 16:06:59 +0200

    story005 Xero oauth2 - Exchange authorisation code for token set

--- backend/src/functions/xeroCreateTokenSet/index.ts
+++ backend/src/functions/xeroCreateTokenSet/index.ts
@@ -1,24 +1,54 @@
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
 import { getScopes, initXeroClient } from '/opt/xero';
 import { AppSyncIdentityCognito, Context } from '@aws-appsync/utils';
 
 const { TABLE_USER } = process.env;
 
-// openid profile email offline_access accounting.transactions accounting.settings accounting.contacts accounting.contacts accounting.attachments
+/**
+ * AWS Lambda handler for updating Xero token in DynamoDB.
+ *
+ * @param {Context} ctx - The context of the Lambda execution containing identity and arguments.
+ * @returns {Promise<object>} An object indicating the success or failure of the operation.
+ */
 export const handler = async (ctx: Context) => {
+  // Extract user identity and input arguments from the context
   const { sub } = ctx.identity as AppSyncIdentityCognito;
   const { input } = ctx.arguments;
   const { url, scopeSet } = input;
   console.log('sub: ', sub);
   console.log('url: ', url);
 
+  // Get the scopes for the Xero client
   const scopes = getScopes(scopeSet);
 
-  let xero;
   try {
-    xero = initXeroClient({
+  // Initialize Xero client and exchange authorization code for token set
+    const xero = initXeroClient({
       scopes: scopes.split(' '),
       grantType: 'authorization_code',
     });
+    // Exchange authorization code for token set
+    const tokenSet = await xero.apiCallback(url);
+
+    // Store the token set in DynamoDB
+    const dynamoDbClient = new DynamoDBClient();
+
+    const updateItemCommand = new UpdateItemCommand({
+      TableName: TABLE_USER,
+      Key: {
+        id: { S: sub },
+      },
+      UpdateExpression: 'SET xeroTokenSet = :tokenSet',
+      ExpressionAttributeValues: {
+        ':tokenSet': { S: JSON.stringify(tokenSet) },
+      },
+    });
+
+    await dynamoDbClient.send(updateItemCommand);
+
+    console.log('Token set updated for user:', tokenSet);
+    
+    return { success: true, message: "Token updated successfully"};
   } catch (err: any) {
     console.log('ERROR init xero: ', err);
     throw new Error(err.message);
--- react-app/src/app/pages/XeroRedirect/XeroRedirect.spec.tsx
+++ react-app/src/app/pages/XeroRedirect/XeroRedirect.spec.tsx
@@ -1,10 +1,40 @@
-import { render } from '../../helpers/render';
+import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import { MemoryRouter } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+import { useTranslation } from 'react-i18next';
 
 import XeroRedirect from './XeroRedirect';
 
+// Mocks for external dependencies
+jest.mock('aws-amplify');
+jest.mock('react-i18next');
+
+// Setup for useTranslation mock
+(useTranslation as jest.Mock).mockReturnValue({
+  t: (key: string) => key,
+});
+
 describe('XeroRedirect', () => {
+  // Mock for Auth.currentAuthenticatedUser
+  const mockAuthUser = () => {
+    (Auth.currentAuthenticatedUser as jest.Mock).mockResolvedValue({
+      email: 'testUser',
+      password: 'testPassword'
+    });
+  };
+
   it('should render successfully', () => {
-    const { baseElement } = render(<XeroRedirect />);
+    mockAuthUser();
+
+    const { baseElement } = render(
+      <MockedProvider>
+        <MemoryRouter>
+          <XeroRedirect />
+        </MemoryRouter>
+      </MockedProvider>
+    );
+
     expect(baseElement).toBeTruthy();
   });
 });
